593513d7605fa9d3a06f4bc343ca37f715c95d16
pabloarista <3964831+pabloarista@users.noreply.github.com>
2025-10-02 14:59:40 -0400

    updated version

--- Sources/MockingbirdCommon/Version.swift
+++ Sources/MockingbirdCommon/Version.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// The current version of Mockingbird.
-public let mockingbirdVersion = Version(shortString: "1.0.2")
+public let mockingbirdVersion = Version(shortString: "1.0.5")
 
 /// A comparable semantic version.
 public struct Version: Comparable, CustomStringConvertible {
